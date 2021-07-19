b7a7de9e676ced7ff9d689ad996040373fabd8c9
mridula-joshi <87464270+mridula-joshi@users.noreply.github.com>
2021-07-19 18:03:28 +0530

    Update communicate_openstack.py

--- Python_communicate_openstack/communicate_openstack.py
+++ Python_communicate_openstack/communicate_openstack.py
@@ -1,19 +1,46 @@
 #!usr/bin/python3
-import sys
+import argparse
+import logging
 import requests
 
-def communicate_api(endpoint,method,api,token):
-        if method not in ['GET','DELETE']:
-                print("Methods Accepted: GET|DELETE")
-                return
-        try:
-                url1=endpoint + api
-                response=requests.request(method=method,url=url1,headers={"X-Auth-Token": token})
-        except Exception as e:
-                print(e)
-                return
-        print(response.text)
+from logging.handlers import RotatingFileHandler
 
-arguments=sys.argv
-communicate_api(arguments[1],arguments[2],arguments[3],arguments[4])
+format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+hdlr = RotatingFileHandler(filename ='opstck.log' , maxBytes=2000 , backupCount=10)
+
+hdlr.setFormatter(format)
+
+logger=logging.getLogger(__name__)
+logging.setLevel(logging.INFO)
+logging.addHandler(hdlr)
+
+class Communicate_with_api:
+        
+        def communicate_api(args):
+                try:
+                        url1=args.endpoint + args.api
+                        response=requests.request(method=args.method,url=url1,headers={"X-Auth-Token": args.token})
+                except request.exceptions.InvalidURL:
+                        logger.error("INVALID URL Error")
+                except request.exceptions.ConnectTimeout:
+                        logger.error("Connection Error")
+                except request.exceptions.HTTPError:
+                        logger.error("HTTP Error")
+                except Exception as e:
+                        logger.warning("Exception %s", e)
+                print(response.headers)
+                print(response.text)
+        
+
+        def main(self):
+        parser=argparse.ArgumentParser()
+        parser.add_argument("endpoint" , help=" Endpoint Url")
+        parser.add_argument("method" , help=" HTTP request method" , choice=['GET','DELETE'] , type= str)
+        parser.add_arument("api" , help="api_ref")
+        parser.add_argument("token" , help="Authorized token")
+        self.communicate_api(parser.parse_args())
+
+if __name__ == "__main__":
+        co=Communicate_with_api()
+        co.main()
