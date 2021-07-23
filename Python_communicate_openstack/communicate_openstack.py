9ef3b39935dfe9518135b5ce484e368b7fd31c00
mridula-joshi <87464270+mridula-joshi@users.noreply.github.com>
2021-07-23 13:34:45 +0530

    Update communicate_openstack.py

--- Python_communicate_openstack/communicate_openstack.py
+++ Python_communicate_openstack/communicate_openstack.py
@@ -1,4 +1,17 @@
-#!usr/bin/python3
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+""" This will help to establish connection and communicate with the   
+    Openstack services using HTTP requests. """
+
 import argparse
 import logging
 import requests
@@ -6,10 +19,11 @@ import requests
 from logging.handlers import RotatingFileHandler
 
 format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-hdlr = RotatingFileHandler(filename ='opstck.log' , maxBytes=2000 , backupCount=10)
-
+hdlr = RotatingFileHandler(filename ='opstck.log' ,
+                           maxBytes=2000 , 
+                           backupCount=10)
 hdlr.setFormatter(format)
+
 
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -20,7 +34,9 @@ class Communicate_with_api:
         def communicate_api(self,args):
                 try:
                         url1=args.endpoint + args.api
-                        response=requests.request(method=args.method,url=url1,headers={"X-Auth-Token": args.token})
+                        response=requests.request(method=args.method ,
+                                                  url=url1 ,
+                                                  headers={"X-Auth-Token": args.token})
                 except request.exceptions.InvalidURL:
                         logger.error("INVALID URL Error")
                 except request.exceptions.ConnectTimeout:
@@ -36,10 +52,16 @@ class Communicate_with_api:
 
         def main(self):
                 parser=argparse.ArgumentParser()
-                parser.add_argument("endpoint" , help=" Endpoint Url")
-                parser.add_argument("method" , help=" HTTP request method" , choices=['GET','DELETE'] , type= str)
-                parser.add_argument("api" , help="api_ref")
-                parser.add_argument("token" , help="Authorized token")
+                parser.add_argument("endpoint" , 
+                                    help=" Endpoint Url")
+                parser.add_argument("method" ,
+                                    help=" HTTP request method" ,
+                                    choices=['GET','DELETE'] ,
+                                    type= str)
+                parser.add_argument("api" ,
+                                    help="api_ref")
+                parser.add_argument("token" ,
+                                    help="Authorized token")
                 self.communicate_api(parser.parse_args())
 
 if __name__ == "__main__":
